41493cc3d2a524b82aa26e0c9a9ec1e68a85b870
AsuraA93 <AsuraA93@gmail.com>
2024-04-29 18:27:48 +0700

    Update service scripts to close and start all services

--- scripts/close_all_services.sh
+++ scripts/close_all_services.sh
@@ -1,7 +1,7 @@
-lsof -t -i:8000 | xargs kill & \
 lsof -t -i:8001 | xargs kill & \
 lsof -t -i:8002 | xargs kill & \
 lsof -t -i:8003 | xargs kill & \
 lsof -t -i:8004 | xargs kill & \
 lsof -t -i:8005 | xargs kill & \
 lsof -t -i:8006 | xargs kill & \
+lsof -t -i:8007 | xargs kill & \
